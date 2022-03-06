0addae82e382fbed70b4dafc99d1a1df8308bfdb
JoeretzYee <yee.it111@gmail.com>
2022-03-06 17:43:10 +0800

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { AppBar, Toolbar, IconButton } from "@material-ui/core";
+import {
+  Menu as MenuIcon,
+  ArrowBack as ArrowBackIcon,
+} from "@material-ui/icons";
+import classNames from "classnames";
+
+// styles
+import useStyles from "./styles";
+
+// components
+import { Typography } from "../Wrappers";
+
+// context
+import {
+  useLayoutState,
+  useLayoutDispatch,
+  toggleSidebar,
+} from "../../context/LayoutContext";
+
+export default function Header(props) {
+  var classes = useStyles();
+
+  // global
+  var layoutState = useLayoutState();
+  var layoutDispatch = useLayoutDispatch();
+
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <IconButton
+          color="inherit"
+          onClick={() => toggleSidebar(layoutDispatch)}
+          className={classNames(
+            classes.headerMenuButtonSandwich,
+            classes.headerMenuButtonCollapse,
+          )}
+        >
+          {layoutState.isSidebarOpened ? (
+            <ArrowBackIcon
+              classes={{
+                root: classNames(
+                  classes.headerIcon,
+                  classes.headerIconCollapse,
+                ),
+              }}
+            />
+          ) : (
+            <MenuIcon
+              classes={{
+                root: classNames(
+                  classes.headerIcon,
+                  classes.headerIconCollapse,
+                ),
+              }}
+            />
+          )}
+        </IconButton>
+        <Typography variant="h6" weight="medium" className={classes.logotype}>
+          St Jude Management System
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
